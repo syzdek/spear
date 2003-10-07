c4272b374b105ee57c2bd0839b313a5cc2fc9b0d
syzdek <syzdek>
2003-10-07 05:09:17 +0000

    -dms ascii readable

--- src/c/encryption/sha1-base64.c
+++ src/c/encryption/sha1-base64.c
@@ -21,50 +21,6 @@
    #include <stdio.h>
 
  	
-char *
-encode (const void *buf, size_t len)
-{
-  /* We know in advance how long the buffer has to be. */
-  unsigned char *in = (unsigned char *) buf;
-  char *out = malloc (6 + ((len + 3) / 4) * 6 + 1);
-  char *cp = out;
-
-  /* Encode the length. */
-  /* Using `htonl' is necessary so that the data can be
-     decoded even on machines with different byte order. */
-
-  cp = mempcpy (cp, l64a (htonl (len)), 6);
-
-  while (len > 3)
-    {
-      unsigned long int n = *in++;
-      n = (n << 8) | *in++;
-      n = (n << 8) | *in++;
-      n = (n << 8) | *in++;
-      len -= 4;
-      if (n)
-        cp = mempcpy (cp, l64a (htonl (n)), 6);
-      else
-            /* `l64a' returns the empty string for n==0, so we 
-               must generate its encoding ("......") by hand. */
-        cp = stpcpy (cp, "......");
-    }
-  if (len > 0)
-    {
-      unsigned long int n = *in++;
-      if (--len > 0)
-        {
-          n = (n << 8) | *in++;
-          if (--len > 0)
-            n = (n << 8) | *in;
-        }
-      memcpy (cp, l64a (htonl (n)), 6);
-      cp += 6;
-    }
-  *cp = '\0';
-  return out;
-}
-
 ///////////////////
 //               //
 // Main Function //
@@ -73,9 +29,10 @@ encode (const void *buf, size_t len)
 int main(int argc, char *argv[]) {
 
    /* Declare local vars */
-      unsigned char *md;
-      unsigned short int pos;
-      char *output;
+      char *md;
+      char pos;
+      char ascii_encode[43];
+      int bin_data;
 
    /* Gets user input */
       if (argc != 2) {
@@ -86,12 +43,23 @@ int main(int argc, char *argv[]) {
    /* Encrypts password */
       SHA1(argv[1], strlen(argv[1]), md);
 
-   /* base64 encode the sha1 data */
-      output = encode(md, (sizeof(char *) * strlen(md)) );
+   /* encodes sha1 in base64 */
+      memset(ascii_encode, 0, 43);
+      for(pos = 0; pos < 8; pos++) {
+         if (md[pos*4] == 0) 
+            md[pos*4] = 1;
+         memcpy(&bin_data, md+(pos*4), 4);
+         memcpy(&ascii_encode[pos*6], l64a(htonl(bin_data)), 6);
+      };
+      ascii_encode[42] = '\0';
+      for(pos = 0; pos < 41; pos++)
+         if (ascii_encode[pos] == 0)
+            ascii_encode[pos] = '0';
 
    /* Displays results */
-      printf("Sha:\t%s\n", output);
-      printf("Sha Size:\t%i\n", strlen(md));
+      printf("Sha1:\t%s\n", ascii_encode);
+      printf("Sha1 Size:\t%i\n", strlen(ascii_encode));
 
-   return(0);
+   /* Ends program */
+      return(0);
 };
