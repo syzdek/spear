7ce916f53923a16d32ef2dd19605ab826c358b5e
syzdek <syzdek>
2003-11-26 09:24:12 +0000

    -dms added example

---
+++ src/c/opengl-guide/example.p6.f1-1.c
@@ -0,0 +1,132 @@
+/*
+ * Alpine Auth Daemon v0.1
+ * $Source$
+ * $Revision$
+ * $Date$
+ * $Author$
+ */
+/*
+ * Copyright (c) 2003 David M. Syzdek <syzdek@mosquitonet.com>
+ * All Rights Reserved.
+ * COPYING RESTRICTIONS APPLY, see LICENSE.TXT file
+ */
+/* From:
+ *    OpenGL Programming Guide (third edition)
+ *    Mason Woo : Jackie Neider : Tom Davis : Dave Shreiner
+ *    Addison Wesley
+ */
+/*  example.p6.f1-1.c - Page 6 - Figure 1.1 */
+/*
+ * Build:
+ *    gcc -g -o example.p6.f1-1 example.p6.f1-1.c -Wall  -lglut -L/usr/X11R6/lib
+ */
+
+
+///////////////
+//           //
+//  Headers  //
+//           //
+///////////////
+
+#include <GL/glut.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//////////////////
+//              //
+//  Prototypes  //
+//              //
+//////////////////
+
+   int  main(int, char **);
+   void example(void);
+   void key(unsigned char key, int x, int y);
+
+
+/////////////////
+//             //
+//  Functions  //
+//             //
+/////////////////
+
+/* Draws Shape */
+void example() {
+   /* OpenGL Examples */
+      glClearColor(0.0, 0.5, 0.0, 0.0);
+      glClear(GL_COLOR_BUFFER_BIT);
+      glColor3f(1.0, 1.0, 1.0);
+      glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+      glBegin(GL_POLYGON);
+         glVertex3f(0.25, 0.25, 0.0);
+         glVertex3f(0.75, 0.25, 0.0);
+         glVertex3f(0.75, 0.75, 0.0);
+         glVertex3f(0.25, 0.75, 0.0);
+      glEnd();
+      glColor3f(0.0, 0.0, 0.5);
+      glBegin(GL_POLYGON);
+         glVertex3f(0.0, 0.0, 0.0);
+         glVertex3f(0.25, 0.0, 0.0);
+         glVertex3f(0.25, 0.25, 0.0);
+         glVertex3f(0.0, 0.25, 0.0);
+      glEnd();
+      glBegin(GL_POLYGON);
+         glVertex3f(0.75, 0.0, 0.0);
+         glVertex3f(1.0, 0.0, 0.0);
+         glVertex3f(1.0, 0.25, 0.0);
+         glVertex3f(0.75, 0.25, 0.0);
+      glEnd();
+      glBegin(GL_POLYGON);
+         glVertex3f(0.75, 0.75, 0.0);
+         glVertex3f(1.0, 0.75, 0.0);
+         glVertex3f(1.0, 1.0, 0.0);
+         glVertex3f(0.75, 1.0, 0.0);
+      glEnd();
+      glBegin(GL_POLYGON);
+         glVertex3f(0.0, 0.75, 0.0);
+         glVertex3f(0.25, 0.75, 0.0);
+         glVertex3f(0.25, 1.0, 0.0);
+         glVertex3f(0.0, 1.0, 0.0);
+      glEnd();
+      glColor3f(0.5, 0.0, 0.0);
+      glBegin(GL_POLYGON);
+         glVertex3f(0.37, 0.37, 0.0);
+         glVertex3f(0.63, 0.37, 0.0);
+         glVertex3f(0.63, 0.63, 0.0);
+         glVertex3f(0.37, 0.63, 0.0);
+      glEnd();
+      glFlush();
+}
+
+
+/* exits on key press */
+void key(unsigned char key, int x, int y) {
+   if (key == 'q') {
+      exit(0);
+     } else {
+      example();
+   };
+}
+
+
+/////////////////////
+//                 //
+//  Main Function  //
+//                 //
+/////////////////////
+int main( int argc, char * argv[] ) {
+
+   /* Window variables  */
+    
+   /* Connect to X server  */
+      glutInitWindowSize(500, 500);
+      glutInitWindowPosition(400, 400);
+      glutInit(&argc, argv);
+      glutCreateWindow("My Test Window");
+      glutDisplayFunc(example);
+      glutKeyboardFunc(key);
+      glutMainLoop();
+
+   /* Ends Function */
+      return(0);
+}
