cb424d6948fedfcdf1344e21fda6c8081142757f
syzdek <syzdek>
2004-03-11 20:07:42 +0000

    -dms added dynamic module examples and global .config include for Makefiles

---
+++ src/c/modules/simple-module.c
@@ -0,0 +1,18 @@
+/*
+ * Example with corrections from:
+ *
+ *    Gary V. Vaughan
+ *    http://sources.redhat.com/autobook/autobook/autobook_163.html#SEC163
+ */  
+
+#include <stdio.h>
+
+int run(const char *);
+
+int
+run (const char *argument)
+{
+  printf ("Hello, %s!\n", argument);
+  return 0;
+}
+
