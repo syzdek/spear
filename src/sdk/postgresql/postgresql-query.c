8d276a44b339bee44282d5616272e065e9d70eb6
David M. Syzdek <syzdek@bindlebinaries.com>
2013-07-17 18:51:46 -0800

    Updating example.

--- src/sdk/postgresql/postgresql-query.c
+++ src/sdk/postgresql/postgresql-query.c
@@ -6,8 +6,10 @@
 int main(int argc, char * argv[]);
 int main(int argc, char * argv[])
 {
-   const char * db   = NULL;
-   PGconn     * pd;
+   PGconn         * pd;
+   PGresult       * res;
+   int              x;
+   int              y;
    char         conninfo[1024];
 
    if (argc < 3)
@@ -15,9 +17,8 @@ int main(int argc, char * argv[])
       fprintf(stderr, "Usage: %s <db>\n", argv[0]);
       return(1);
    };
-   db = argv[1];
 
-   snprintf(conninfo, 1024, "host=localhost dbname=%s", db);
+   snprintf(conninfo, 1024, "host=localhost dbname=%s", argv[1]);
    if ((pd = PQconnectdb(conninfo)) == NULL)
    {
       fprintf(stderr, "%s: PQconnectdb(): Unable to initialize database library.", argv[0]);
@@ -30,6 +31,51 @@ int main(int argc, char * argv[])
       PQfinish(pd);
       return(1);
    };
+
+   res = PQexec(pd, argv[2]);
+   if ((!res) || ((PQresultStatus(res) != PGRES_TUPLES_OK)))
+   {
+      fprintf(stderr, "%s: PQexec(): %s\n", argv[0], PQerrorMessage(pd));
+      PQclear(res);
+      PQfinish(pd);
+      return(1);
+   };
+
+   printf("%i rows found\n", PQntuples(res));
+   printf("%i columns in rows\n", PQnfields(res));
+
+   for(y = 0; y < PQnfields(res); y++)
+      printf("%s, ", PQfname(res, y));
+   printf("\n");
+
+   for(x = 0; x < PQntuples(res); x++)
+   {
+      for(y = 0; y < PQnfields(res); y++)
+      {
+         if ((PQgetisnull(res, x,y)))
+         {
+            printf("NULL, ");
+            continue;
+         };
+         switch(PQftype(res, y))
+         {
+            case 25:
+            printf("text");
+            break;
+
+            case VARCHAROID:
+            printf("varchar");
+            break;
+
+            default:
+            break;
+         };
+         printf("%s, ", PQgetvalue(res, x, y));
+      };
+      printf("\n");
+   };
+
+   PQclear(res);
 
    PQfinish(pd);
 
